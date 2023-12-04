88bf5956a2e9788292bd282f8e4fdabc66f64e59
Kevin Diaz <diazk2@wit.edu>
2023-12-03 23:34:36 -0500

    Create 543-Diameter of Binary Tree.py

---
+++ src/neetcode/trees/543-Diameter of Binary Tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from idlelib.tree import TreeNode
+from typing import Optional
+
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def height(root):
+            nonlocal diameter
+            if not root:
+                return 0
+
+            left = height(root.left)
+            right = height(root.right)
+            diameter = max(diameter, left + right)
+            return max(left, right) + 1
+
+        diameter = 0
+        height(root)
+        return diameter
